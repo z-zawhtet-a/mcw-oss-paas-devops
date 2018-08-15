a2905dd24ed078b21e608eefe1d8d57db8be1996
z-zawhtet-a <z.zawhtet.a@ieee.org>
2018-08-15 07:06:58 +0000

    ADD Header, CHANGE .dockerignore

--- src/components/plan/Plans.js
+++ src/components/plan/Plans.js
@@ -58,6 +58,7 @@ export default class Plans extends Component {
 
         return (
             <div class="container">
+                <h3>Welcome to Best For You Organics Company</h3>
                 <Grid>
                     <div class="plans">
                         <Row>{planCatalog}</Row>
